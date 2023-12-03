1fd276e3a3b5ff8b6745cdb20f75dfdafced5484
Penpen <71316821+penpen-uwu@users.noreply.github.com>
2023-12-02 19:57:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 register("command", (...arg) => {
-    if (arg == undefined || arg.join(" ") == "help") {
+    if (arg == undefined || arg.join(" ").removeFormatting() == "help" || arg.join(" ").removeFormatting() == "") {
         ChatLib.chat(ChatLib.getChatBreak("&e&m "));
         ChatLib.chat("/hs <search>");
         ChatLib.chat(ChatLib.getChatBreak("&e&m "));
     } else {
         test = new Thread(() => {
-            ChatLib.chat(`Searching for \"${arg.join(" ")}\"...`);
-            let houses = JSON.parse(FileLib.getUrlContent(`https://housing.menu/houses?search=${arg.join("+")}&amount=5&orderby=id`));
+            ChatLib.chat(`Searching for \"${arg.join(" ").removeFormatting()}\"...`);
+            let houses = JSON.parse(FileLib.getUrlContent(`https://housing.menu/houses?search=${arg.join("+").removeFormatting()}&amount=5&orderby=last_updated`));
             if (houses["total"] == 0) {
                 ChatLib.chat("No houses found")
             } else {
                 ChatLib.chat(ChatLib.getChatBreak("&e&m "));
                 houses["rows"].forEach((element) => {
-                    ChatLib.chat(`${element["name"]}&r, by ${element["creator"]}`);
-                    msg = new TextComponent("&e&lClick here to join!").setClick("run_command", `/visit ${element["creator"].removeFormatting().replace(/\[[\w+\+-]+] /, "").trim()} ${element["name"].removeFormatting()}`).setHover("show_text", `Click here to run /visit ${element["creator"].removeFormatting().replace(/\[[\w+\+-]+] /, "").trim()} ${element["name"].removeFormatting()}`)
+                    if (element["alive"]) {
+                        indicator = "&a●";
+                    } else {
+                        indicator = "&c●";
+                    }
+                    ChatLib.chat(`${element["name"]}&r, by ${element["creator"]} ${indicator}`);
+                    msg = new TextComponent("&e&lClick here to join!").setClick("run_command", `/visit ${element["creator"].removeFormatting().replace(/\[[\w+\+-]+] /, "").trim()} ${element["name_raw"]}`).setHover("show_text", `Click here to run /visit ${element["creator"].removeFormatting().replace(/\[[\w+\+-]+] /, "").trim()} ${element["name_raw"]}`)
                     ChatLib.chat(msg);
                     ChatLib.chat("");
                 });
